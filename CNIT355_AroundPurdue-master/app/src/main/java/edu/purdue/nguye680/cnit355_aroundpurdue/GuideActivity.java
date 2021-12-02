03872566c694652b65dbf7e68350a614df6f740a
Xerrishades <42281925+Xerrishades@users.noreply.github.com>
2021-12-02 01:09:03 -0500

    Added back-end functionality
    Added location data and dynamic display for locations

---
+++ CNIT355_AroundPurdue-master/app/src/main/java/edu/purdue/nguye680/cnit355_aroundpurdue/GuideActivity.java
@@ -0,0 +1,42 @@
+package edu.purdue.nguye680.cnit355_aroundpurdue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+
+        TextView lblMain = (TextView)findViewById(R.id.lblMain);
+        lblMain.setText(MainActivity.locations.get(MainActivity.place).name);
+
+        TextView txtRate = (TextView)findViewById(R.id.txtRate);
+        txtRate.setText(MainActivity.locations.get(MainActivity.place).rate);
+
+        TextView txtFilter = (TextView)findViewById(R.id.txtFilter);
+        txtFilter.setText(MainActivity.locations.get(MainActivity.place).filter);
+
+        TextView txtDescription = (TextView)findViewById(R.id.txtDescription);
+        txtDescription.setText(MainActivity.locations.get(MainActivity.place).description);
+
+        TextView txtContact = (TextView)findViewById(R.id.txtContact);
+        txtContact.setText(txtContact.getText() + MainActivity.locations.get(MainActivity.place).contact);
+
+        TextView txtPrice = (TextView)findViewById(R.id.txtPrice);
+        txtPrice.setText(txtPrice.getText() + MainActivity.locations.get(MainActivity.place).price);
+
+        TextView txtLocation = (TextView)findViewById(R.id.txtLocation);
+        txtLocation.setText(txtLocation.getText() + MainActivity.locations.get(MainActivity.place).location);
+
+        TextView txtTime = (TextView)findViewById(R.id.txtTime);
+        txtTime.setText(txtTime.getText() + MainActivity.locations.get(MainActivity.place).time);
+
+        TextView txtRecommendation = (TextView)findViewById(R.id.txtRecommendation);
+        txtRecommendation.setText(txtRecommendation.getText() + MainActivity.locations.get(MainActivity.place).recommendation);
+    }
+}
---
+++ CNIT355_AroundPurdue-master/app/src/main/java/edu/purdue/nguye680/cnit355_aroundpurdue/Location.java
@@ -0,0 +1,25 @@
+package edu.purdue.nguye680.cnit355_aroundpurdue;
+
+public class Location {
+    String name;
+    String filter;
+    String recommendation;
+    String description;
+    String price;
+    String rate;
+    String location;
+    String address;
+    String time;
+    String contact;
+    String link;
+    String image;
+
+    static Location A = new Location();
+    static Location B = new Location();
+    static Location C = new Location();
+    static Location D = new Location();
+    static Location E = new Location();
+    static Location F = new Location();
+    static Location G = new Location();
+    static Location H = new Location();
+}
